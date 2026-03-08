ed7be9aaa0fece4babe7f96db5854a5a1ff2361a
Jerod Hollen <jerod.a.hollen@gmail.com>
2026-03-08 14:15:35 -0700

    Add solution for Merge Strings Alternately

---
+++ solutions/easy/Merge Strings Alternately.cpp
@@ -0,0 +1,22 @@
+// Title: Merge Strings Alternately
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/merge-strings-alternately/
+
+        while(i < n1 || i < n2) {
+            if(i < n1) {
+                merged += word1[i];
+            }
+            if(i < n2) {
+                merged += word2[i];
+            }
+
+        int i = 0;
+        int n2 = word2.length();
+        int n1 = word1.length();
+        string merged = "";
+        
+    string mergeAlternately(string word1, string word2) {
+public:
+class Solution {
+            i++;
