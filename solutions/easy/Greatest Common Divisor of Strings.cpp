28da4787b199fc245c9846e1b10268bba593015e
Jerod Hollen <jerod.a.hollen@gmail.com>
2026-03-08 23:33:03 -0700

    Update Greatest Common Divisor of Strings.cpp

--- solutions/easy/Greatest Common Divisor of Strings.cpp
+++ solutions/easy/Greatest Common Divisor of Strings.cpp
@@ -4,7 +4,7 @@
             // Link: https://leetcode.com/problems/greatest-common-divisor-of-strings/
 public:
 class Solution {
-string gcdOfStrings(string str1, string str2) {
+   string gcdOfStrings(string str1, string str2) {
         // Check if str1 + str2 == str2 + str1
         if(str1 + str2 != str2 + str1) {
             return "";
