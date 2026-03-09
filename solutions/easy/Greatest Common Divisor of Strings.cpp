36febd595b1ada319662003c9ca7e370060ee705
Jerod Hollen <jerod.a.hollen@gmail.com>
2026-03-08 23:37:47 -0700

    Update Greatest Common Divisor of Strings.cpp

--- solutions/easy/Greatest Common Divisor of Strings.cpp
+++ solutions/easy/Greatest Common Divisor of Strings.cpp
@@ -1,21 +1,16 @@
-// Title: Greatest Common Divisor of Strings
-            // Difficulty: Easy
-            // Language: C++
-            // Link: https://leetcode.com/problems/greatest-common-divisor-of-strings/
+class Solution {
 public:
-class Solution {
-   string gcdOfStrings(string str1, string str2) {
-        // Check if str1 + str2 == str2 + str1
-        if(str1 + str2 != str2 + str1) {
-            return "";
-        }
-        
-        
-        int n1 = str1.length();
-        int n2 = str2.length();
+    string gcdOfStrings(string str1, string str2) {
+        
+        // Check if str1 + str2 == str2 + str1
+        if(str1 + str2 != str2 + str1) {
+            return "";
+        }
+        
+        int n1 = str1.length();
+        int n2 = str2.length();
 
-        int gcd = std::gcd(n1, n2);
-        return str1.substr(0,gcd);
-    }
+        int gcd = std::gcd(n1, n2);
+        return str1.substr(0,gcd);
+    }
 };
-
